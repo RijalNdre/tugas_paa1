1480629873fa43a02e953104d2bccc5a28ddd553
Rizal Andrias <93868952+RijalNdre@users.noreply.github.com>
2023-04-06 19:43:42 +0700

    Update hallogin.js

--- src/pages/hallogin.js
+++ src/pages/hallogin.js
@@ -10,7 +10,7 @@ export default function login() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Register
+                Login
               </h1>
               <form method='POST' action="/api/login"  className="space-y-4 md:space-y-6" >
                 <div>
